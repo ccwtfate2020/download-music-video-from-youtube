06cb660d01b84c19098d8d982e8e140c4a36fb23
ccwtfate2020 <ccwtfate2020@gmail.com>
2023-05-27 13:57:03 +0800

    v1.0.1

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # Download Music video from Youtube
-test
+N/A
 
 ## Getting Started
-test
+
+1. Build a docker image<br>
+```docker build -t my-python3-container:1.0 .```
+
+2. Download music<br>
+```docker compose run download-music```<br>
+   <br>OR<br><br>
+   Download video<br>
+```docker compose run download-video```
+
+
 ### Dependencies
-test
+N/A
 
 ## Version History
 
-Version | Date | Description
---- | --- | ---
-v1.0.0 | 11 May 2023 | Initial version
-
+| Version | Date | Description
+| --- | --- | ---
+| v1.0.0 | 11 May 2023 | Initial version
+| v1.0.1 | 27 May 2023 | 1. Change the output path in docker-compose.yaml and main.py
+|        |             | 2. Update the download music and download video function in main.py
 
 ## Reference
-
+N/A
 
 
 
--- main.py
+++ main.py
@@ -5,40 +5,25 @@ import subprocess
 import os
 
 warning="\033[93m"
+error="\033[31m"
+msg="\033[34m"
 default="\033[0m"
+
+music_dir = '~/Music/temp'
+video_dir = '~/Videos/temp'
+output_dir = '/app/output'
+youtube_link_filename="youtubeLink.txt"
 
 try:
     choice = argv[1]
 except:
     exit(f"Please input as following: \n{warning}python3 {argv[0]} (mp3/mp4){default}\n")
-# link = "https://www.youtube.com/watch?v=7jna9bvhdRE&list=PLdR7m7PFLzQ7RqOVfxk2Fr2F-a7iWzovn&index=3"
-
-# music_dir = '/home/ccwtadmin/Music/temp'
-# video_dir = '/home/ccwtadmin/Videos/temp'
-music_dir = '/app/output'
-video_dir = '/app/output'
 
 # Function
-def download_mp3(link,retry = 0):
-    # yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-    yt = YouTube(link)
-    while retry < 3:
-        try:
-            video = yt.streams.get_audio_only()
-            break
-        except:
-            retry += 1
-            time.sleep(3)
-            print(f"{retry} retry")
-
+def download_mp3(mp4_filename):
     try:
-        mp4_filename = video.default_filename
         mp3_filename = os.path.splitext(mp4_filename)[0] + '.mp3'
-
-        print(f"Download \"{mp4_filename}\" into {music_dir}")
-        video.download(music_dir)
-
-        print(f"Covert \"{mp4_filename}\" to \"{mp3_filename}\"")
+        print(f"{msg}Covert \"{mp4_filename}\" to \"{mp3_filename}\" ...{default}")
         # ffmpeg man page
         # https://linux.die.net/man/1/ffmpeg
 
@@ -48,13 +33,13 @@ def download_mp3(link,retry = 0):
         subprocess.run([
             'ffmpeg',
             '-y',
-            '-i', os.path.join(music_dir, mp4_filename),
-            os.path.join(music_dir, mp3_filename)
+            '-i', os.path.join(output_dir, mp4_filename),
+            os.path.join(output_dir, mp3_filename)
         ])
         #Remove mp4 file
         subprocess.run([
             'rm',
-            os.path.join(music_dir, mp4_filename)
+            os.path.join(output_dir, mp4_filename)
         ])
     except:
         raise Exception
@@ -62,41 +47,38 @@ def download_mp3(link,retry = 0):
 
 
 def download_mp4(link,retry = 0):
+    # yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
     yt = YouTube(link)
     # filter mp4 streams from object
     while retry < 3:
         try:
             video = yt.streams.get_by_resolution(resolution="720p")
-            break
-        except:
-            retry += 1
-            time.sleep(3)
-            print(f"{retry} retry")
-
-    while retry < 3:
-        try:
             mp4_filename = video.default_filename
-            print(f"Download \"{mp4_filename}\" into {video_dir}")
-            video.download(video_dir)
-            break
+            print(f"{msg}Download \"{mp4_filename}\" ...{default}")
+            video.download(output_dir)
+            return mp4_filename
         except:
             retry += 1
             time.sleep(3)
-            print(f"{retry} retry to download {mp4_filename}")
+            print(f"{error}{retry} retry to download {mp4_filename}{default}")
     else:
-        exit(f"Failed to download {mp4_filename}")
+        exit(f"{error}Failed to download {mp4_filename}{default}")
 
 #Using readlines()
-file = open('youtubeLink.txt', 'r')
+file = open(youtube_link_filename, 'r')
 lines = file.readlines()
 
+if not lines:
+    exit(f"{error}\"{youtube_link_filename}\" is empty. Please input youtube link in \"{youtube_link_filename}\"{default}")
 
 if(choice=="mp3"):
     for link in lines:
-        download_mp3(link)
+        mp4_filename = download_mp4(link)
+        download_mp3(mp4_filename)
+    print(f"{msg}Please check directory \"{music_dir}\"{default}")
 elif(choice=="mp4"):
     for link in lines:
         download_mp4(link)
-    #download_mp4(lines[0])
+    print(f"{msg}Please check directory \"{video_dir}\"{default}")
 else:
-    print(f"Please input mp3 or mp4")
+    exit(f"Please input as following: \n{warning}python3 {argv[0]} (mp3/mp4){default}\n")
